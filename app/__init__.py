724df560a9d14dec70124648c9e17764fde8921a
Astro <programadorastro@gmail.com>
2025-11-12 21:47:59 -0300

    add Talisman and Proxy_FIX

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 from app.db import db
 from flask_mail import Mail, Message
 import secrets
-
-
+from werkzeug.middleware.proxy_fix import ProxyFix
+from flask_talisman import Talisman
 load_dotenv()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
@@ -18,6 +18,8 @@ app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME') 
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD') 
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER') 
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+Talisman(app, force_https=True)
 mail = Mail(app)
 db.init_app(app)
 
--- app/utils.py
+++ app/utils.py
@@ -1,17 +1,22 @@
 #envio de email
 from flask_mail import Message
-from app import mail
+from app import mail, app
 import secrets
 
 def enviar_email(destinatario, assunto, mensagem_html):
-    msg = Message(
-        subject=assunto,
-        recipients=[destinatario],
-        html=mensagem_html
-    )
-    mail.send(msg)
 
 
+    try:
+        msg = Message(
+            subject=assunto,
+            sender=app.config['MAIL_USERNAME'],
+            recipients=[destinatario],
+            html=mensagem_html
+        )
+        mail.send(msg)
+        print("✅ Email enviado com sucesso!")
+    except Exception as e:
+        print("❌ Falha ao enviar email:", e)
 
 def gerar_token_seguro():
     return str(secrets.randbelow(9000) + 1000)
