b4aecfc1d1042f7352f32a87651d3533574a3a81
Astro <programadorastro@gmail.com>
2025-11-12 22:48:27 -0300

    fix mail and wsgi

--- app/__init__.py
+++ app/__init__.py
@@ -7,20 +7,24 @@ from flask_mail import Mail, Message
 import secrets
 from werkzeug.middleware.proxy_fix import ProxyFix
 from flask_talisman import Talisman
+
+
 load_dotenv()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'CHAVESECRETA'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME') 
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD') 
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER') 
-#app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 mail = Mail(app)
 db.init_app(app)
+
+
 
 csp = {
     'default-src': [
@@ -46,7 +50,10 @@ csp = {
     ]
 }
 
-#Talisman(app, force_https=True, content_security_policy=csp)
+if os.getenv("RAILWAY_ENVIRONMENT") == "production":
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+    Talisman(app, force_https=True, content_security_policy=csp)
+
 from app.models import Comentario
 with app.app_context():
     db.create_all()
